c6fba64070840937b55a6bbbe72eecb48b5ad2de
UriKH <urikh@icloud.com>
2025-11-11 15:46:29 +0200

    init

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -7,6 +7,7 @@ int main() {
     // read tokens until the end of file is reached
     while ((token = static_cast<tokentype>(yylex()))) {
         // your code here
+        output::printToken(yylineno, token, yytext);
     }
     return 0;
 }
--- HW1/scanner.lex
+++ HW1/scanner.lex
@@ -1,9 +1,14 @@
 %{
     #include "tokens.hpp"
+
 }%
+
+letter [a-zA-Z]
+digit  [0-9]
 
 %%
 
-
+void { return VOID; }
+/* TODO: insert all other stuff */
 
 %%
