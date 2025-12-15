3ea8895f7e6db9b5ea40606fe0fa858fdad096e7
shiraz <shiraz.shmulman@gmail.com>
2025-12-15 23:41:26 +0200

    HW3 what we did

---
+++ HW3/output.cpp
@@ -0,0 +1,219 @@
+#include "output.hpp"
+#include <iostream>
+
+namespace output {
+    /* Helper functions */
+
+    static std::string toString(ast::BuiltInType type) {
+        switch (type) {
+            case ast::BuiltInType::INT:
+                return "int";
+            case ast::BuiltInType::BOOL:
+                return "bool";
+            case ast::BuiltInType::BYTE:
+                return "byte";
+            case ast::BuiltInType::VOID:
+                return "void";
+            case ast::BuiltInType::STRING:
+                return "string";
+            default:
+                return "unknown";
+        }
+    }
+
+    /* Error handling functions */
+
+    void errorLex(int lineno) {
+        std::cout << "line " << lineno << ": lexical error\n";
+        exit(0);
+    }
+
+    void errorSyn(int lineno) {
+        std::cout << "line " << lineno << ": syntax error\n";
+        exit(0);
+    }
+
+    void errorUndef(int lineno, const std::string &id) {
+        std::cout << "line " << lineno << ":" << " variable " << id << " is not defined" << std::endl;
+        exit(0);
+    }
+
+    void errorDefAsFunc(int lineno, const std::string &id) {
+        std::cout << "line " << lineno << ":" << " symbol " << id << " is a function" << std::endl;
+        exit(0);
+    }
+
+    void errorDefAsVar(int lineno, const std::string &id) {
+        std::cout << "line " << lineno << ":" << " symbol " << id << " is a variable" << std::endl;
+        exit(0);
+    }
+
+    void errorDef(int lineno, const std::string &id) {
+        std::cout << "line " << lineno << ":" << " symbol " << id << " is already defined" << std::endl;
+        exit(0);
+    }
+
+    void errorUndefFunc(int lineno, const std::string &id) {
+        std::cout << "line " << lineno << ":" << " function " << id << " is not defined" << std::endl;
+        exit(0);
+    }
+
+    void errorMismatch(int lineno) {
+        std::cout << "line " << lineno << ":" << " type mismatch" << std::endl;
+        exit(0);
+    }
+
+    void errorPrototypeMismatch(int lineno, const std::string &id, std::vector<std::string> &paramTypes) {
+        std::cout << "line " << lineno << ": prototype mismatch, function " << id << " expects parameters (";
+
+        for (int i = 0; i < paramTypes.size(); ++i) {
+            std::cout << paramTypes[i];
+            if (i != paramTypes.size() - 1)
+                std::cout << ",";
+        }
+
+        std::cout << ")" << std::endl;
+        exit(0);
+    }
+
+    void errorUnexpectedBreak(int lineno) {
+        std::cout << "line " << lineno << ":" << " unexpected break statement" << std::endl;
+        exit(0);
+    }
+
+    void errorUnexpectedContinue(int lineno) {
+        std::cout << "line " << lineno << ":" << " unexpected continue statement" << std::endl;
+        exit(0);
+    }
+
+    void errorMainMissing() {
+        std::cout << "Program has no 'void main()' function" << std::endl;
+        exit(0);
+    }
+
+    void errorByteTooLarge(int lineno, const int value) {
+        std::cout << "line " << lineno << ": byte value " << value << " out of range" << std::endl;
+        exit(0);
+    }
+
+    /* ScopePrinter class */
+
+    ScopePrinter::ScopePrinter() : indentLevel(0) {}
+
+    std::string ScopePrinter::indent() const {
+        std::string result;
+        for (int i = 0; i < indentLevel; ++i) {
+            result += "  ";
+        }
+        return result;
+    }
+
+    void ScopePrinter::beginScope() {
+        indentLevel++;
+        buffer << indent() << "---begin scope---" << std::endl;
+    }
+
+    void ScopePrinter::endScope() {
+        buffer << indent() << "---end scope---" << std::endl;
+        indentLevel--;
+    }
+
+    void ScopePrinter::emitVar(const std::string &id, const ast::BuiltInType &type, int offset) {
+        buffer << indent() << id << " " << toString(type) << " " << offset << std::endl;
+    }
+
+    void ScopePrinter::emitFunc(const std::string &id, const ast::BuiltInType &returnType,
+                                const std::vector<ast::BuiltInType> &paramTypes) {
+        globalsBuffer << id << " " << "(";
+
+        for (int i = 0; i < paramTypes.size(); ++i) {
+            globalsBuffer << toString(paramTypes[i]);
+            if (i != paramTypes.size() - 1)
+                globalsBuffer << ",";
+        }
+
+        globalsBuffer << ")" << " -> " << toString(returnType) << std::endl;
+    }
+
+    std::ostream &operator<<(std::ostream &os, const ScopePrinter &printer) {
+        os << "---begin global scope---" << std::endl;
+        os << printer.globalsBuffer.str();
+        os << printer.buffer.str();
+        os << "---end global scope---" << std::endl;
+        return os;
+    }
+
+    MyVisitor::MyVisitor() {}
+
+    void MyVisitor::visit(ast::ID &node) {}
+
+    void MyVisitor::visit(ast::If &node) {}
+
+    void MyVisitor::visit(ast::Or &node) {}
+
+    void MyVisitor::visit(ast::And &node) {}
+
+    void MyVisitor::visit(ast::Not &node) {}
+
+    void MyVisitor::visit(ast::Num &node) {}
+
+    void MyVisitor::visit(ast::Bool &node) {}
+
+    void MyVisitor::visit(ast::Call &node) {}
+
+    void MyVisitor::visit(ast::Cast &node) {}
+
+    void MyVisitor::visit(ast::NumB &node) {}
+
+    void MyVisitor::visit(ast::Type &node) {}
+
+    void MyVisitor::visit(ast::BinOp &node) {}
+
+    void MyVisitor::visit(ast::Break &node) {}
+
+    void MyVisitor::visit(ast::Funcs &node) {}
+
+    void MyVisitor::visit(ast::RelOp &node) {}
+
+    void MyVisitor::visit(ast::While &node) {}
+
+    void MyVisitor::visit(ast::Assign &node) {}
+
+    void MyVisitor::visit(ast::Formal &node) {}
+
+    void MyVisitor::visit(ast::Return &node) {}
+
+    void MyVisitor::visit(ast::String &node) {}
+
+    void MyVisitor::visit(ast::ExpList &node) {}
+
+    void MyVisitor::visit(ast::Formals &node) {}
+
+    void MyVisitor::visit(ast::VarDecl &node) {
+        /**
+         * 1. get new offset
+         * 2. check if exists table[id][i].offset < new offset (if true error)
+         * 3. add to scope stack + add to symbol table + offset stack
+         * 4. (if new type, add to type table)
+         */
+        // compute new offsets
+        int offset = -1;
+        if (!offset_stack.empty()) {
+            offset = offset_stack.top()
+        }
+        // check existence
+        if (symbolTable.find(node.value) == symbolTable.end()) { //not found
+            symbolTable[node.value] = SymbolTableRow(node.value, {offset + 1})
+        }
+
+        // add to scope sack
+
+        symbolTable[node.value] =
+    }
+
+    void MyVisitor::visit(ast::Continue &node) {}
+
+    void MyVisitor::visit(ast::FuncDecl &node) {}
+
+    void MyVisitor::visit(ast::Statements &node) {}
+}
---
+++ HW3/output.hpp
@@ -0,0 +1,143 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+#include <stack>
+#include "visitor.hpp"
+#include "nodes.hpp"
+
+namespace output {
+    /* Error handling functions */
+
+    void errorLex(int lineno);
+
+    void errorSyn(int lineno);
+
+    void errorUndef(int lineno, const std::string &id);
+
+    void errorDefAsFunc(int lineno, const std::string &id);
+
+    void errorUndefFunc(int lineno, const std::string &id);
+
+    void errorDefAsVar(int lineno, const std::string &id);
+
+    void errorDef(int lineno, const std::string &id);
+
+    void errorPrototypeMismatch(int lineno, const std::string &id, std::vector<std::string> &paramTypes);
+
+    void errorMismatch(int lineno);
+
+    void errorUnexpectedBreak(int lineno);
+
+    void errorUnexpectedContinue(int lineno);
+
+    void errorMainMissing();
+
+    void errorByteTooLarge(int lineno, int value);
+
+    /* ScopePrinter class
+     * This class is used to print scopes in a human-readable format.
+     */
+    class ScopePrinter {
+    private:
+        std::stringstream globalsBuffer;
+        std::stringstream buffer;
+        int indentLevel;
+
+        std::string indent() const;
+
+    public:
+        ScopePrinter();
+
+        void beginScope();
+
+        void endScope();
+
+        void emitVar(const std::string &id, const ast::BuiltInType &type, int offset);
+
+        void emitFunc(const std::string &id, const ast::BuiltInType &returnType,
+                      const std::vector<ast::BuiltInType> &paramTypes);
+
+        friend std::ostream &operator<<(std::ostream &os, const ScopePrinter &printer);
+    };
+
+    class MyVisitor : public Visitor {
+    private:
+        struct SymbolTableRow {
+            std::string name;
+            // TODO: extrac properties
+
+            SymbolTableRow(const std::string)
+        };
+
+        struct Scope{
+            std::stack<std::string> scope;
+        };
+
+        ScopePrinter scopePrinter;
+
+        std::stack<Scope> scope_stack;
+        std::stack<int> offset_stack;
+        std::map<std::string, std::vector<SymbolTableRow>> symbolTable;
+
+    public:
+        MyVisitor();
+
+        void visit(ast::Num &node) override;
+
+        void visit(ast::NumB &node) override;
+
+        void visit(ast::String &node) override;
+
+        void visit(ast::Bool &node) override;
+
+        void visit(ast::ID &node) override;
+
+        void visit(ast::BinOp &node) override;
+
+        void visit(ast::RelOp &node) override;
+
+        void visit(ast::Not &node) override;
+
+        void visit(ast::And &node) override;
+
+        void visit(ast::Or &node) override;
+
+        void visit(ast::Type &node) override;
+
+        void visit(ast::Cast &node) override;
+
+        void visit(ast::ExpList &node) override;
+
+        void visit(ast::Call &node) override;
+
+        void visit(ast::Statements &node) override;
+
+        void visit(ast::Break &node) override;
+
+        void visit(ast::Continue &node) override;
+
+        void visit(ast::Return &node) override;
+
+        void visit(ast::If &node) override;
+
+        void visit(ast::While &node) override;
+
+        void visit(ast::VarDecl &node) override;
+
+        void visit(ast::Assign &node) override;
+
+        void visit(ast::Formal &node) override;
+
+        void visit(ast::Formals &node) override;
+
+        void visit(ast::FuncDecl &node) override;
+
+        void visit(ast::Funcs &node) override;
+    };
+}
+
+#endif //OUTPUT_HPP
